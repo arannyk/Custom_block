ab50117e799035e5f23edcdeaeefbf8201eb6ff4
Md Arannyk Monon <monon12@gmail.com>
2018-11-17 02:47:16 +0600

    Add files via upload

--- block.js
+++ block.js
@@ -1,68 +1,26 @@
-/**
- * Block Styles Example
- *
- * https://github.com/arannyk/Custom_block/
- */
-( function() {
-	var __                = wp.i18n.__; // The __() function for internationalization.
-	var createElement     = wp.element.createElement; // The wp.element.createElement() function to create elements.
-	var registerBlockType = wp.blocks.registerBlockType; // The registerBlockType() function to register blocks.
-	var RichText          = wp.editor.RichText; // For creating editable elements.
+const { __ } = wp.i18n;
+const { registerBlockType } = wp-blocks;
 
-	/**
-	 * Register block
-	 *
-	 * @param  {string}   name     Block name.
-	 * @param  {Object}   settings Block settings.
-	 * @return {?WPBlock}          Block itself, if registered successfully,
-	 *                             otherwise "undefined".
-	 */
-	registerBlockType(
-		'mon/block-styles-example', // Block name. Must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-		{
-			title: __( 'Block Styles Example' ), // Block title. __() function allows for internationalization.
-			icon: 'admin-appearance', // Block icon from Dashicons. https://developer.wordpress.org/resource/dashicons/.
-			category: 'common', // Block category. Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
-			attributes: {
-				content: {
-					type: 'array',
-					source: 'children',
-					selector: 'p',
-					default: 'Block content styled with CSS...'
-				},
-			},
+const blockStyle = {
+	backgroundColor: '#ccc',
+	color: '#fff',
+	padding: '20px'
+}
 
-			// Defines the block within the editor.
-			edit: function( props ) {
-				var content = props.attributes.content;
+// my-plugin/block-name
 
-				function onChangeContent( updatedContent ) {
-					props.setAttributes( { content: updatedContent } );
-				}
+registerBlockType( 'basic-gutenberg-block/block-example-01-basic-block', {
 
-				return createElement(
-					RichText,
-					{
-						tagName: 'p',
-						className: props.className,
-						value: content,
-						onChange: onChangeContent
-					},
-				);
-			},
+	title: __( 'Basic Blcok' ),
+	icon: 'universal-access-alt'
+	category: 'layout',
+	// Used in editor
+	edit(){
+		return <div style={ blackStyle }> This Is The Basic Block with JSX! (editor) </div>;
+	},
+	// Used in the frontend
+	save() {
+		return <div style={ blockStyle }> Basic Example With JSX! (frontend) </div>;
+	}
 
-			// Defines the saved block.
-			save: function( props ) {
-				var content = props.attributes.content;
-
-				return createElement(
-					'p',
-					{
-						className: props.className,
-					},
-					content
-				);
-			},
-		}
-	);
-})();
+});
