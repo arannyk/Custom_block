43478bb6b0f53c4c0ae6dfa55fad109a5f0c46bd
Md Arannyk Monon <monon12@gmail.com>
2018-11-17 17:52:28 +0600

    Add files via upload

---
+++ basic-gutenberg-block/basic-gutenberg-block.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Custom Gutenberg Block
+ * Plugin URI: https://github.com/
+ * Description: A example Gutenberg block with static content.
+ * Author: Md Arannyk Monon
+ * Author URI: 
+ * Version: 1.0.0
+ * License: GPL2+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+//Prevent direct access
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+include '01-basic-block/index.php';
+
+ 
